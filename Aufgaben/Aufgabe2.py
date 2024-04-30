98644e69a120345e3069f677756fc5c0512d2411
LouisD-github <148058478+LouisD-github@users.noreply.github.com>
2024-04-30 11:50:02 +0200

    Add files via upload

--- Aufgaben/Aufgabe2.py
+++ Aufgaben/Aufgabe2.py
@@ -1,1 +1,43 @@
-...
+from random import *
+min = int(input("Geben Sie die Zahlen in Minuten an: "))
+h = min//60
+min = min%60
+sek = min%1
+min = min - sek
+sek = sek*60
+sek = int(sek)
+print(h, "h", min, "min", sek, "sek")
+
+print()
+
+print("Errate eine Zah von 1 bis 100: ")
+z = randint(1,100)
+print(z)
+erraten = False
+while erraten == False :
+    r = input()
+    if r.isnumeric() : 
+        r = int(r)
+        if r == z :
+            print("Erraten!")
+            erraten = True
+        if r > z : 
+            print("Eingabe ist zu groß!")
+        if r < z :
+            print("Eingabe ist zu klein!")
+    else :
+        print("Eingabe ist keine Zahl!")
+
+print(" ")
+
+s = input("Gebe zufällige Buchstaben und Zahlen ein: ")
+zahlen = 0
+buchstaben = ""
+for char in s:
+    if char.isnumeric():
+        zahlen += int(char)
+    else:
+        buchstaben += char
+    
+print(zahlen, buchstaben)
+input("Done")
